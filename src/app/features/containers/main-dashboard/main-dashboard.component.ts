c55d8072b018ff3cbbeb002dcd5d6ca4718167af
TomcikKometa <tomcik48_84@o2.pl>
2024-05-10 14:17:12 +0200

    add cpomonent to main dashorad

--- src/app/features/containers/main-dashboard/main-dashboard.component.ts
+++ src/app/features/containers/main-dashboard/main-dashboard.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NavBarComponent } from '../../components/nav-bar/nav-bar.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { DashboardCardComponent } from '../../components/dashboard-card/dashboard-card.component';
 
 @Component({
   selector: 'app-main-dashboard',
   standalone: true,
-  imports: [NavBarComponent],
+  imports: [NavBarComponent,DashboardCardComponent],
   templateUrl: './main-dashboard.component.html',
   styleUrl: './main-dashboard.component.scss'
 })
