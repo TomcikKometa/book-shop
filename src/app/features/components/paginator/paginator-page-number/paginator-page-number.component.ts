d6a6608d160bc80c08ad24ae60c31b6be25b42b1
TomcikKometa <tomcik48_84@o2.pl>
2024-05-17 12:04:27 +0200

    develop paginator components

--- src/app/features/components/paginator/paginator-page-number/paginator-page-number.component.ts
+++ src/app/features/components/paginator/paginator-page-number/paginator-page-number.component.ts
@@ -11,4 +11,5 @@ import { Component, Input } from '@angular/core';
 export class PaginatorPageNumberComponent {
   @Input() public pageNumber: number = 1;
   @Input() public isActive: boolean = false;
+  @Input() public index: number = 0;
 }
--- src/app/features/components/paginator/paginator.component.ts
+++ src/app/features/components/paginator/paginator.component.ts
@@ -9,7 +9,7 @@ import {
 } from '@angular/core';
 import { PaginatorPageNumberComponent } from './paginator-page-number/paginator-page-number.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
 import { Observable, Subject } from 'rxjs';
 import { MainDashboardService } from '../../../api/services/main-dashboard.service';
@@ -32,16 +32,21 @@ interface PaginatorSelectOptions {
     PaginatorPageNumberComponent,
     MatFormFieldModule,
     CommonModule,
-    MatIconModule
+    MatIconModule,
   ],
   templateUrl: './paginator.component.html',
   styleUrls: ['./paginator.component.scss'],
 })
 export class PaginatorComponent implements OnInit {
   public selectedNumberOfItems: number = 10;
-  public pages: number[] = [];
-  public numberOfPages$: Observable<number> = inject(MainDashboardService).numberOfPages;
-  public numberOfPages:number = 0
+  public pages: any[] = [];
+  increasedPage = 10;
+  decreasedPage = 1;
+  start = 0;
+  protected pagesToHandle: number = 0;
+  public numberOfPages$: Observable<number> =
+    inject(MainDashboardService).numberOfPages;
+  public numberOfPages: number = 0;
   @Input() public currentPage: number = 1;
   public changedPage$: Subject<number> = new Subject<number>();
 
@@ -51,20 +56,33 @@ export class PaginatorComponent implements OnInit {
     { label: '20', value: 20 },
   ];
 
+  public paginationObject = {
+    previousValue: 0,
+    currentValue: 1
+  };
+
   private readonly distroyReference: DestroyRef = inject(DestroyRef);
-  private readonly mainDashboardService: MainDashboardService = inject(MainDashboardService);
+  private readonly mainDashboardService: MainDashboardService =
+    inject(MainDashboardService);
 
   public ngOnInit(): void {
-    this.numberOfPages$.pipe(takeUntilDestroyed(this.distroyReference))
-      .subscribe((value:number) => this.numberOfPages = value)
-    this.pages = Array(this.numberOfPages)
-      .fill(0)
+    this.numberOfPages$
+      .pipe(takeUntilDestroyed(this.distroyReference))
+      .subscribe((value: number) => {
+        this.pagesToHandle = value;
+        this.pages = Array(10)
+      .fill(10)
       .map((x: unknown, i: number) => i + 1);
+        this.preparePagesToLoad();
+      });
   }
 
   public changePage(page: number): void {
+    this.paginationObject.previousValue = this.currentPage
+    this.paginationObject.currentValue = page;
     this.currentPage = page;
     this.handlePaginationChange();
+
   }
 
   public moveBackwardPage(): void {
@@ -82,6 +100,27 @@ export class PaginatorComponent implements OnInit {
   }
 
   public handlePaginationChange(): void {
-    this.mainDashboardService.changedPage(this.currentPage)
+    this.mainDashboardService.changedPage(this.currentPage);
+  }
+
+  preparePagesToLoad() {
+
+      if(this.paginationObject.previousValue < this.paginationObject.currentValue){
+        let start = this.increasedPage++;
+        let removeInxdex = this.increasedPage - 11;
+        this.pages = Array(start).fill(0).map((x: unknown, i: number) => i + 1);
+        this.pages.splice(0,removeInxdex);
+      }
+
+      if(this.paginationObject.previousValue > this.paginationObject.currentValue){
+        let start = this.increasedPage--;
+        this.pages = Array(this.increasedPage).fill({},2).map((x: unknown, i: number) => i - 1);
+        if(this.pages.length >= 12 && this.pages.indexOf(0) != 1){
+          const lengthDiffrence = this.pages.length - 10;
+          this.pages.splice(0,lengthDiffrence);
+        } else {
+          this.pages = Array(10).fill(0).map((x: unknown, i: number) => i + 1);
+        }
+      }
   }
 }
