25820901196952ae3d3f8d72e1a4b67a008f5cb7
TomcikKometa <tomcik48_84@o2.pl>
2024-06-05 13:48:58 +0200

    add select input,change rendering of card in dashboard

--- src/app/features/components/dashboard-card/dashboard-card.component.ts
+++ src/app/features/components/dashboard-card/dashboard-card.component.ts
@@ -1,10 +1,4 @@
-import {
-  Component,
-  OnChanges,
-  OnInit,
-  DestroyRef,
-  inject,
-} from '@angular/core';
+import { Component, OnChanges, OnInit, DestroyRef, inject, ViewChild, ChangeDetectorRef } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { Observable, tap } from 'rxjs';
@@ -14,19 +8,34 @@ import { CommonModule } from '@angular/common';
 import { BookInfoService } from '../../../api/services/api-book-info/book-info.service';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { BookInfoComponent } from '../book-info/book-info.component';
+import { AnimationOptions, LottieComponent } from 'ngx-lottie';
+import lottie from 'lottie-web';
 @Component({
   selector: 'app-dashboard-card',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule,CommonModule,MatDialogModule],
+  imports: [MatCardModule, MatButtonModule, CommonModule, MatDialogModule, LottieComponent],
   templateUrl: './dashboard-card.component.html',
-  styleUrl: './dashboard-card.component.scss',
+  styleUrl: './dashboard-card.component.scss'
 })
-export class DashboardCardComponent{
+export class DashboardCardComponent {
+  options: AnimationOptions = {
+    path: '/assets/animations/loading.json',
+    loop: true,
+    autoplay: true
+  };
+  protected isRendered: boolean = true;
   public bookPerPage$: Observable<ApiBookModel[]> = inject(MainDashboardService).booksPerPage;
   public isSpinner: Observable<boolean> = inject(MainDashboardService).isSpinner;
   public isData: Observable<boolean> = inject(MainDashboardService).isData;
+  public changeDetectionRef:ChangeDetectorRef = inject(ChangeDetectorRef);
+
+  @ViewChild('#lottieComponent') lottiComponent? : LottieComponent;
 
   protected readonly bookInfoService: BookInfoService = inject(BookInfoService);
 
- 
+  complited() {
+    this.isRendered = false;
+    this.changeDetectionRef.detectChanges();
+    console.log(this.isRendered);
+  }
 }
