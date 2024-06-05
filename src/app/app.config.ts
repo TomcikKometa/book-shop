79fd0a88002aef122b3316a8be7a93dad9f49b15
TomcikKometa <tomcik48_84@o2.pl>
2024-06-05 15:12:09 +0200

    delete observable

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,9 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
+import player from 'lottie-web';
+import { provideLottieOptions } from 'ngx-lottie';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync(),provideHttpClient()]
+  providers: [provideRouter(routes), provideAnimationsAsync(),provideHttpClient(),provideLottieOptions({ player: () => player }),]
 };
