d6ee3e3261f68bbd731ad8a70993ff16a1117fc4
TomcikKometa <tomcik48_84@o2.pl>
2024-06-12 11:31:28 +0200

    refactor code

--- src/app/features/components/nav-bar/nav-bar.component.ts
+++ src/app/features/components/nav-bar/nav-bar.component.ts
@@ -1,7 +1,7 @@
 import { Component, DestroyRef, OnInit, inject } from '@angular/core';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { debounceTime, distinctUntilChanged } from 'rxjs';
-import { MainDashboardService,SearchType } from '../../../api/services/main-dashboard.service';
+import { MainDashboardService, SearchType } from '../../../api/services/main-dashboard.service';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
@@ -9,32 +9,25 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
   standalone: true,
   imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.scss',
+  styleUrl: './nav-bar.component.scss'
 })
 export class NavBarComponent implements OnInit {
   protected searchControlTitle: FormControl = new FormControl<string>('');
   protected searchControlAuthor: FormControl = new FormControl<string>('');
   protected searchControlEpoch: FormControl = new FormControl<string>('');
 
-  private readonly mainDashboardService: MainDashboardService =
-    inject(MainDashboardService);
+  private readonly mainDashboardService: MainDashboardService = inject(MainDashboardService);
   private readonly distroyReference: DestroyRef = inject(DestroyRef);
 
   public ngOnInit(): void {
     this.searchControlTitle.valueChanges
-      .pipe(takeUntilDestroyed(this.distroyReference),debounceTime(500),distinctUntilChanged())
-      .subscribe((value: string) =>
-        this.mainDashboardService.searchBooks(value,SearchType.SEARCH_TITLE),
-      );
-      this.searchControlAuthor.valueChanges
-      .pipe(takeUntilDestroyed(this.distroyReference),debounceTime(500),distinctUntilChanged())
-      .subscribe((value: string) =>
-        this.mainDashboardService.searchBooks(value,SearchType.SEARCH_AUTHOR),
-      );
-      this.searchControlEpoch.valueChanges
-      .pipe(takeUntilDestroyed(this.distroyReference),debounceTime(500),distinctUntilChanged())
-      .subscribe((value: string) =>
-        this.mainDashboardService.searchBooks(value,SearchType.SEARCH_EPOCH),
-      );
+      .pipe(takeUntilDestroyed(this.distroyReference), debounceTime(500), distinctUntilChanged())
+      .subscribe((value: string) => this.mainDashboardService.searchBooks(value, SearchType.SEARCH_TITLE));
+    this.searchControlAuthor.valueChanges
+      .pipe(takeUntilDestroyed(this.distroyReference), debounceTime(500), distinctUntilChanged())
+      .subscribe((value: string) => this.mainDashboardService.searchBooks(value, SearchType.SEARCH_AUTHOR));
+    this.searchControlEpoch.valueChanges
+      .pipe(takeUntilDestroyed(this.distroyReference), debounceTime(500), distinctUntilChanged())
+      .subscribe((value: string) => this.mainDashboardService.searchBooks(value, SearchType.SEARCH_EPOCH));
   }
 }
