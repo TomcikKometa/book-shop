22250e54dfae8c399db5fcfcc099ec8d5825b769
TomcikKometa <tomcik48_84@o2.pl>
2024-06-12 10:20:13 +0200

    change modyficators in functions

--- src/app/features/components/book-info/book-info.component.ts
+++ src/app/features/components/book-info/book-info.component.ts
@@ -1,17 +1,15 @@
-import { ChangeDetectorRef, Component, DestroyRef, OnInit, afterRender, inject } from '@angular/core';
-import { Observable, debounceTime, distinctUntilChanged, first } from 'rxjs';
+import { ChangeDetectorRef, Component, DestroyRef, OnInit, inject } from '@angular/core';
+import { Observable, debounceTime, distinctUntilChanged } from 'rxjs';
 import { BookInfoService } from '../../../api/services/api-book-info/book-info.service';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { MatDialogConfig, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { ApiSingleBookInfoDto } from '../../../api/services/models/api-response-single-bookDto-model';
 import { AnimationOptions, LottieComponent } from 'ngx-lottie';
-import lottie from 'lottie-web';
 import { MatFormField, MatSelectModule } from '@angular/material/select';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { SearchType } from '../../../api/services/main-dashboard.service';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
@@ -34,9 +32,7 @@ import { MatIconModule } from '@angular/material/icon';
   styleUrl: './book-info.component.scss'
 })
 export class BookInfoComponent implements OnInit {
-  backgroundImage: string = '';
-  x = true;
-  options: AnimationOptions = {
+  protected options: AnimationOptions = {
     path: '/assets/animations/loading.json',
     loop: true,
     autoplay: true
@@ -45,38 +41,30 @@ export class BookInfoComponent implements OnInit {
   protected isRendered: boolean = false;
   protected isImageLoading: boolean = true;
 
-  public bookInfo$: Observable<ApiSingleBookInfoDto> = inject(BookInfoService).apiResponseSingleInfo;
-  public dialogRef: MatDialogRef<BookInfoComponent> = inject(MatDialogRef);
-  public changeDetectionRef: ChangeDetectorRef = inject(ChangeDetectorRef);
+  protected readonly bookInfo$: Observable<ApiSingleBookInfoDto> = inject(BookInfoService).apiResponseSingleInfo;
+  private readonly dialogRef: MatDialogRef<BookInfoComponent> = inject(MatDialogRef);
+  private readonly changeDetectionRef: ChangeDetectorRef = inject(ChangeDetectorRef);
   private readonly distroyReference: DestroyRef = inject(DestroyRef);
-  selectedCompetence: any;
 
-  ngOnInit(): void {
-    this.bookInfo$.pipe(first()).subscribe((value: ApiSingleBookInfoDto) => {
-      this.backgroundImage = value.cover;
-      console.log(value);
-    });
-
+  public ngOnInit(): void {
     this.searchControlTitle.valueChanges
       .pipe(takeUntilDestroyed(this.distroyReference), debounceTime(500), distinctUntilChanged())
       .subscribe((value: string) => {
-        console.log(value),
-          value === 'Wybierz MP3' ? '' : (window.location.href = value),
-          console.log(window.location.href);
+          value === 'Wybierz MP3' ? '' : (window.location.href = value)
       });
   }
-  protected load(event: any) {
+  protected load() {
     this.isImageLoading = false;
   }
 
-  complited(event: any) {
+  protected complited() {
     if (this.isImageLoading === false) {
       this.isRendered = true;
       this.changeDetectionRef.detectChanges();
     }
   }
 
-  addToBusket() {
+  protected addToBusket() {
    window.alert('ssss');
   }
 
