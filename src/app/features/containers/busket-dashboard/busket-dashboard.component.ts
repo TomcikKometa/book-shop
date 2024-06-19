2d773e97b97dd0f4493caa8ad64b7c4570b245fd
TomcikKometa <tomcik48_84@o2.pl>
2024-06-19 10:50:19 +0200

    add new component to features

---
+++ src/app/features/containers/busket-dashboard/busket-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, inject } from '@angular/core';
+import { BusketService } from '../../../api/services/busket/busket.service';
+
+@Component({
+  selector: 'app-busket-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './busket-dashboard.component.html',
+  styleUrl: './busket-dashboard.component.scss'
+})
+export class BusketDashboardComponent {
+
+ 
+
+}
