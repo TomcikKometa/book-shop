2d6f9ebffd1b400901c8a41f8ec9c56b276e5773
TomcikKometa <tomcik48_84@o2.pl>
2024-06-19 10:47:16 +0200

    add path to busket component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainDashboardComponent } from './features/containers/main-dashboard/main-dashboard.component';
+import { NavBarComponent } from './features/components/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,MainDashboardComponent],
+  imports: [RouterOutlet,MainDashboardComponent,NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
